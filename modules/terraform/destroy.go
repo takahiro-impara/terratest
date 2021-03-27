624be6f1a566623bc192b562b92257b87eab92f8
takahiro-impara <takahiro4120@gmail.com>
2021-03-27 12:39:34 +0900

    mod destroy option for tf 0.11

--- modules/terraform/destroy.go
+++ modules/terraform/destroy.go
@@ -21,7 +21,7 @@ func TgDestroyAll(t testing.TestingT, options *Options) string {
 
 // DestroyE runs terraform destroy with the given options and return stdout/stderr.
 func DestroyE(t testing.TestingT, options *Options) (string, error) {
-	return RunTerraformCommandE(t, options, FormatArgs(options, "destroy", "-auto-approve", "-input=false")...)
+	return RunTerraformCommandE(t, options, FormatArgs(options, "destroy", "-force", "-input=false")...)
 }
 
 // TgDestroyAllE runs terragrunt destroy with the given options and return stdout.
